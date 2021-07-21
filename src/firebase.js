e04f0056c87d5a4b072fb42f81caa1c03d9a2fd6
Merdoth <ucheya97@gmail.com>
2021-07-21 23:26:38 +0100

    initial Application setup

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyA7T4WHfdFzttOEsWgVbKBtDerssWn0Ta4",
+  authDomain: "tiktok-wish.firebaseapp.com",
+  projectId: "tiktok-wish",
+  storageBucket: "tiktok-wish.appspot.com",
+  messagingSenderId: "372149584435",
+  appId: "1:372149584435:web:18f1af9efa2922ceaed946",
+  measurementId: "G-9EMPL6XYX3"
+};
